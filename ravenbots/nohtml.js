8c239480d30e44e9c7456cb7980b5bf331d680fd
Humayd Khan <81250612+NotDyamuh@users.noreply.github.com>
2025-04-13 10:29:13 -0500

    added no rightclick

--- ravenbots/nohtml.js
+++ ravenbots/nohtml.js
@@ -3,3 +3,7 @@ if (path.endsWith('.html')) {
   const cleanPath = path.replace(/\.html$/, '');
   window.history.replaceState(null, '', cleanPath);
 }
+
+document.addEventListener('contextmenu', function(event) {
+    event.preventDefault();
+});
