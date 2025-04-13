aa96f652069a9e6fa8d7fc250446283c84e3ee73
Humayd Khan <81250612+NotDyamuh@users.noreply.github.com>
2025-04-13 10:32:49 -0500

    Update nohtml.js

--- ravenbots/nohtml.js
+++ ravenbots/nohtml.js
@@ -7,3 +7,18 @@ if (path.endsWith('.html')) {
 document.addEventListener('contextmenu', function(event) {
     event.preventDefault();
 });
+
+document.onkeydown = function(e) {
+    if (e.ctrlKey && (e.keyCode === 67 || e.keyCode === 86 || e.keyCode === 85 || e.keyCode === 117)) {
+        return false;
+    } else {
+        return true;
+    }
+};
+$(document).keypress("u", function(e) {
+    if (e.ctrlKey) {
+        return false;
+    } else {
+        return false;
+    }
+});
